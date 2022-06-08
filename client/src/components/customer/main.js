fd4e322775c1291d6992f46e05097cf253710094
Virthiya98 <virthiya.uthayanan98@gmail.com>
2022-06-08 04:32:17 -0400

    JUS-36: Added navigation bar to organizer page

--- client/src/components/customer/main.js
+++ client/src/components/customer/main.js
@@ -8,9 +8,9 @@ function CustomerMain() {
                 <ul>
                     <li><a id="Home" href="/customer/main">Home</a></li>
                     <li><a id="Starred" href="/customer/main">Starred</a></li>
-                    <li><input id="searchbar" type="text" placeholder="Search..."></input></li>
+                    <li><input id="Searchbar" type="text" placeholder="Search..."></input></li>
                     <li><a id="Profile" href="/customer/main">Profile</a></li>
-                    <li><a id="Logout" href="/customer/main">Logout</a></li>
+                    <li><a id="Logout" href="/login">Logout</a></li>
                 </ul>
             </div>
             <h1 id="title"> Customer main page </h1>
