4b1c37a8e011f655c4fcc6632d0548e9ee8a7bde
Virthiya98 <virthiya.uthayanan98@gmail.com>
2022-06-08 04:15:14 -0400

    JUS-36: Began adding a navigation bar on the main customer page

--- client/src/components/customer/main.js
+++ client/src/components/customer/main.js
@@ -3,8 +3,17 @@ import "./main.css";
 
 function CustomerMain() {
     return (
-        <div className="CustomerMain">
-            <h1 id="Title"> Customer main page </h1>
+        <div>
+            <div class="topnav">
+                <ul>
+                    <li><a id="Home" href="/customer/main">Home</a></li>
+                    <li><a id="Starred" href="/customer/main">Starred</a></li>
+                    <li><input id="searchbar" type="text" placeholder="Search..."></input></li>
+                    <li><a id="Profile" href="/customer/main">Profile</a></li>
+                    <li><a id="Logout" href="/customer/main">Logout</a></li>
+                </ul>
+            </div>
+            <h1 id="title"> Customer main page </h1>
         </div>
     );
 
