08638a94a3b22321e81d4a9d63c434da8c812709
bhoomi221 <48841472+bhoomi221@users.noreply.github.com>
2022-06-20 20:42:13 -0400

    JUS-69: merging conflicts from dev

--- server/routes/customerRoutes.js
+++ server/routes/customerRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 import { registerCustomer, loginCustomer, verifyEmail, resendCode } from '../controllers/customerController.js';
 import { validateUserRegisterSchema, validateUserLoginSchema } from "../util/validation/userValidationSchema.js";
+import { customerInfo } from '../controllers/customerInfoController.js';
 
 const customerRouter = express.Router();
 
@@ -9,5 +10,6 @@ customerRouter.route("/").post(validateUserRegisterSchema, registerCustomer);
 customerRouter.route("/").put(validateUserLoginSchema, loginCustomer);
 customerRouter.route("/verifyemail/:email/:token").post(verifyEmail);
 customerRouter.route("/resendcode").post(resendCode);
+customerRouter.route("/personal-info/:email").get(customerInfo); 
 
 export { customerRouter }
