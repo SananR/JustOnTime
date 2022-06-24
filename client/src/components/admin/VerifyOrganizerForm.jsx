8cf25f5aac6cfdf13cb3a23aa857c665185e8a38
Yuto Omachi <youomachi@gmail.com>
2022-06-23 22:48:57 -0400

    added fade in animation

--- client/src/components/admin/VerifyOrganizerForm.jsx
+++ client/src/components/admin/VerifyOrganizerForm.jsx
@@ -1,7 +1,5 @@
 import React, {useEffect, useState} from "react";
-import { CSSTransition } from 'react-transition-group';
 import {useSelector, useDispatch, getState} from 'react-redux'
-import { store } from "../../store";
 import { loadOrganizers, verifyOrganizer } from "../../features/verifyOrganizers/verifyOrganizerSlice"
 import VerifyOrganizerNode from './node/verifyOrganizerNode'
 import './verifyOrganizerForm.css'
@@ -37,7 +35,7 @@ function VerifyOrganizerForm(props){
     return (
         <div id="verify-organizer-container" class="mt-5 mb-5 p-5 shadow-lg container">
             <h1 id="verify-organizer-text" class="p-5 text-center"><strong>List of Organizers to Verify</strong></h1>
-                <div id="organizers" class="animated fadeInDown">
+                <div id="organizers">
                 {organizerList()}
                 </div>
         </div>
