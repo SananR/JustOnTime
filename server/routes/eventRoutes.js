bc7cd9af764ebc913dc4afad9de238f2dc53733f
Arya Sharma <73757085+Arya2504s@users.noreply.github.com>
2022-06-27 17:19:08 -0400

    JUS-107 Completed update events backend api

--- server/routes/eventRoutes.js
+++ server/routes/eventRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { addEvent, getEvents, getOrganizerEvents} from '../controllers/eventController.js';
+import { addEvent, getEvents, getOrganizerEvents, updateEvents} from '../controllers/eventController.js';
 import { eventImageService } from '../util/multer.js';
 import { checkAuthentication} from '../util/passport/authentication.js';
 import { validateEventCreationSchema } from '../util/validation/eventValidationSchema.js';
@@ -10,5 +10,6 @@ const eventRouter = express.Router();
 eventRouter.route("/").post( checkAuthentication(['Organizer']),eventImageService.uploadImage, validateEventCreationSchema, addEvent);
 eventRouter.route("/").get( checkAuthentication([]),getEvents);
 eventRouter.route("/organizerEvents").get( checkAuthentication([]),getOrganizerEvents);
+eventRouter.route("/updateEvent").post(checkAuthentication(['Organizer']), eventImageService.uploadImage, validateEventCreationSchema, updateEvents);
 
 export { eventRouter }
