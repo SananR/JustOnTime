da1808c9fb394728bbd5b8e471be709a7077e607
Yuto Omachi <youomachi@gmail.com>
2022-06-26 21:28:02 -0400

    added a backend api to change the status of the event

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 
-import { getUnverifiedOrganizers, updateOrganizerStatus, getUnverifiedEvents } from '../controllers/adminController.js';
+import { getUnverifiedOrganizers, updateOrganizerStatus, getUnverifiedEvents, updateEventStatus } from '../controllers/adminController.js';
 
 const adminRouter = express.Router();
 
 adminRouter.route("/getUnverifiedOrganizers").get(getUnverifiedOrganizers);
 adminRouter.route("/updateOrganizerStatus").post(updateOrganizerStatus);
 adminRouter.route("/getUnverifiedEvents").get(getUnverifiedEvents);
+adminRouter.route("/updateEventStatus").post(updateEventStatus);
 
 export { adminRouter }
