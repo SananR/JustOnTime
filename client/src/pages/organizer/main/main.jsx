d3bf49bf80b65397916bbfe59157e51fb90f981b
Virthiya98 <virthiya.uthayanan98@gmail.com>
2022-06-29 18:02:29 -0400

    changed the code to use useEffect

--- client/src/pages/organizer/main/main.jsx
+++ client/src/pages/organizer/main/main.jsx
@@ -1,55 +1,52 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import "./main.css";
 import logo from './logo.png';
 
-export default class OrganizerMain extends React.Component {
+function OrganizerMain() {
 
-    constructor(props) {
-        super(props);
-        this.state = { events: [] };
-    }
+    const state = { events: [] };
 
-    componentDidMount() {
+    useEffect(() => {
         axios.get("/organizerEvents")
         .then(res => {
             this.setState({events: res.data});
         })
-    }
+    }, [])
 
-    render() {
-        return (
-            <div>
-                <div className="top">
-                    <h1 id="title"> My Events </h1>
-                    <button id="createEvent">New Event</button>
-                </div>
-                <div className="list">
-                    {
-                        this.state.events
-                        .map(event =>
-                            <button class="event">
-                                {event.eventImage_path}<br/>
-                                {event.name}<br/>
-                                {event.time}<br/>
-                                {event.address.street}<br/>
-                                {event.address.city}<br/>
-                                {event.address.country}
-                            </button>
-                        )
-                    }
-                    {/* remove once events are added */}
-                    <button class="event">
-                        <img src={logo} alt='JustOnTime' width="150" height="100"/><br/>
-                        Event 1<br/>
-                        January 1, 2022<br/>
-                        6:00 p.m.<br/>
-                        123 Main Street<br/>
-                        Toronto<br/>
-                        Canada
-                    </button>
-                </div>
+    return (
+        <div>
+            <div className="top">
+                <h1 id="title"> My Events </h1>
+                <button id="createEvent">New Event</button>
             </div>
-        );
-    }
+            <div className="list">
+                {
+                    state.events
+                    .map(event =>
+                        <button className="event">
+                            {event.eventImage_path}<br/>
+                            {event.name}<br/>
+                            {event.time}<br/>
+                            {event.address.street}<br/>
+                            {event.address.city}<br/>
+                            {event.address.country}
+                        </button>
+                    )
+                }
+                {/* remove once events are added */}
+                <button className="event">
+                    <img src={logo} alt='JustOnTime' width="150" height="100"/><br/>
+                    Event 1<br/>
+                    January 1, 2022<br/>
+                    6:00 p.m.<br/>
+                    123 Main Street<br/>
+                    Toronto<br/>
+                    Canada
+                </button>
+            </div>
+        </div>
+    );
 }
+
+export default OrganizerMain
