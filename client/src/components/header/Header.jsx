95605c52b37c36a75785b6cb33f92d0a54ffb6e6
Virthiya98 <virthiya.uthayanan98@gmail.com>
2022-06-30 19:57:01 -0400

    updated code to include cropped logo and converted code to use useEffect to get events

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import Navbar from 'react-bootstrap/Navbar'
 import UserButton from './userbutton/UserButton'
 
-import logo from './logo.png'
+import logo from '../../logo_cropped.png'
 
 import './header.css'
 
--- client/src/store.js
+++ client/src/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from './features/auth/authSlice'
-import eventsReducer from './features/event/eventSlice'
 
 export const store = configureStore({
     reducer: {
-        auth: authReducer,
-        events: eventsReducer
+        auth: authReducer
     }
 })
