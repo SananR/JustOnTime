ac0628a045931164fc257877332db7a2363aa45e
bhoomi221 <48841472+bhoomi221@users.noreply.github.com>
2022-06-30 21:25:03 -0400

    JUS-74 Made the SIdebar UI more modular so it can be reused

---
+++ client/src/components/setting-sidebar/settingSidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './SettingSidebar.css'
+
+//update the bar as we add more features then it will be uniform for all settings pages 
+
+function SettingSidebar() {
+    return (
+        <div id="sidebar-cont">
+            <a href='/personal-info' > <span id='sidebar-nav'> Personal Information</span></a>
+         </div>
+    );
+}
+
+
+export default SettingSidebar; 
